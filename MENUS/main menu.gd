7a53c9376f22dffe204bfa6f3550469425d162ad
MayjorMayhem <bschaedig@gmail.com>
2020-05-02 12:29:57 -0400

    linked main menu

--- MENUS/main menu.gd
+++ MENUS/main menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	$HBoxContainer/VBoxContainer/Play_button.grab_focus()
 
 func _on_Play_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Everything Else/World.tscn")
 
 func _on_Info_button_pressed():
 	get_tree().change_scene("res://MENUS/info_menu.tscn")
